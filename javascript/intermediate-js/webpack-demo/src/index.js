d11610178dae1728c1869743efa3a101388d42c2
Keshav K <kkeshav1998@gmail.com>
2024-03-04 01:18:44 -0800

    webpack-loaders: loading css & fonts

--- javascript/intermediate-js/webpack-demo/src/index.js
+++ javascript/intermediate-js/webpack-demo/src/index.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import Img from './unsplash.jpg';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myImg = new Image();
+  myImg.src = Img;
+
+  element.appendChild(myImg);
+
   return element;
 }
 
