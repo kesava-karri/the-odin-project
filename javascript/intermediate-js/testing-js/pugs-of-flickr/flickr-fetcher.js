7501cdcbc90ac6d2d091b4d611881913c3be83b0
Keshav K <kkeshav1998@gmail.com>
2025-03-09 23:12:26 -0700

    testing-js

---
+++ javascript/intermediate-js/testing-js/pugs-of-flickr/flickr-fetcher.js
@@ -0,0 +1,12 @@
+var FlickrFetcher = {
+  photoObjToURL: function (photoObj) {
+    return ['https://farm',
+      photoObj.farm, '.staticflickr.com/',
+      photoObj.server, '/',
+      photoObj.id, '_',
+      photoObj.secret, '_b.jpg',
+    ].join('');
+  }
+};
+
+module.exports = FlickrFetcher;
